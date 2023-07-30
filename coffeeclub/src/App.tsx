678383a40f7de7fef5a52e07cf2b093dd1cc7b51
gary-mcmonagle <gary.mcmonagle@15gifts.com>
2023-07-30 13:28:57 +0100

    lil proovements

--- coffeeclub/src/App.tsx
+++ coffeeclub/src/App.tsx
@@ -3,37 +3,16 @@ import logo from "./logo.svg";
 // import { DevTest, sayHello, BeanApi } from "@gary-mcmonagle/coffeeclubapi";
 import "./App.css";
 import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Router } from "./features/routing/Router";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+import { AuthProvider } from "./features/auth/AuthProvider";
 
 export const App = () => {
   return (
-    <GoogleOAuthProvider clientId="28478423072-hdocp28c46djj7ov6976j5mlprdkvkq9.apps.googleusercontent.com">
-      <Router />
-    </GoogleOAuthProvider>
-
-    // <GoogleOAuthProvider clientId="28478423072-hdocp28c46djj7ov6976j5mlprdkvkq9.apps.googleusercontent.com">
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <GoogleLogin
-    //         onSuccess={(suc) => {
-    //           console.log({ suc });
-    //         }}
-    //         onError={() => {
-    //           console.log("err");
-    //         }}
-    //       />
-    //     </header>
-    //   </div>
-    // </GoogleOAuthProvider>
+    <AuthProvider>
+      <GoogleOAuthProvider clientId="28478423072-hdocp28c46djj7ov6976j5mlprdkvkq9.apps.googleusercontent.com">
+        <Router />
+      </GoogleOAuthProvider>
+    </AuthProvider>
   );
 };
 
