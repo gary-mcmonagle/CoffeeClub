3849c6592e7d3002707c737277c3eeeac7ff22e9
gary-mcmonagle <gary.mcmonagle@15gifts.com>
2023-08-22 21:45:52 +0100

    Add converter to enums

--- API/CoffeeClub.Domain/Enumerations/OrderStatus.cs
+++ API/CoffeeClub.Domain/Enumerations/OrderStatus.cs
@@ -1,11 +1,14 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace CoffeeClub.Domain.Enumerations;
 
+[JsonConverter(typeof(StringEnumConverter))]
 public enum OrderStatus
 {
     Pending,
     Received,
     Assigned,
-
     InProgress,
     Ready
 }
--- API/CoffeeClub.Domain/Enumerations/Roast.cs
+++ API/CoffeeClub.Domain/Enumerations/Roast.cs
@@ -1,4 +1,9 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace CoffeeClub.Domain.Enumerations;
+
+[JsonConverter(typeof(StringEnumConverter))]
 
 public enum Roast
 {
