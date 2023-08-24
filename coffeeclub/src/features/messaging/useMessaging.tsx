4f299e4526934d69b160d92e6076e5db08b9e22b
gary-mcmonagle <gary.mcmonagle@15gifts.com>
2023-08-24 07:54:36 +0100

    Hack out signalr code (for now!)

--- coffeeclub/src/features/messaging/useMessaging.tsx
+++ coffeeclub/src/features/messaging/useMessaging.tsx
@@ -8,5 +8,7 @@ export type OrderUpdateDto = {
 
 export const useMessaging = () => {
   const { connection } = useContext(MessagingContext);
-  return { connection };
+  let d1 = new Date();
+  let d2 = new Date("1995-12-17T03:24:00");
+  return { connection: d1 === d2 ? connection : undefined };
 };
