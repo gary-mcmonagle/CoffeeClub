45640246e3f9ff7e2c99f4d3a3c10f3a09c05ac7
gary-mcmonagle <gary.mcmonagle@15gifts.com>
2023-09-03 14:53:47 +0100

    fix build

--- API/CoffeeClub.Domain/Repositories/IUserRepository.cs
+++ API/CoffeeClub.Domain/Repositories/IUserRepository.cs
@@ -7,5 +7,5 @@ public interface IUserRepository : IBaseRepository<User>
 {
     Task<User?> GetAsync(string id, AuthProvider authProvider);
     Task<User?> GetOrCreateAsync(string id, AuthProvider authProvider, bool isWorker);
-    Task<Guid?> GetUserId(string authId, AuthProvider authProvider, bool isWorker)
+    Task<Guid?> GetUserId(string authId, AuthProvider authProvider, bool isWorker);
 }
